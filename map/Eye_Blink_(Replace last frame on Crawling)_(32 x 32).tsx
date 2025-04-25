4fd350138e838c08e4a792a96b567a3dc2317f19
sergej <skoprov@icloud.com>
2025-04-25 14:39:08 +0300

    a lil more things added

---
+++ map/Eye_Blink_(Replace last frame on Crawling)_(32 x 32).tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Eye_Blink_(Replace last frame on Crawling)_(32 x 32)" tilewidth="32" tileheight="32" tilecount="1" columns="1">
+ <image source="../Sprite Pack 5/6 - Mr. Chomps/Eye_Blink_(Replace last frame on Crawling)_(32 x 32).png" width="32" height="32"/>
+</tileset>
